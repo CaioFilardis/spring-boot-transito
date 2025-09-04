33bebba38814e22293d5deaa197c9130f5e5cb2a
Caio Filardis <caiosantosshake@hotmail.com>
2025-09-04 13:34:48 -0300

    Criando o projeto

---
+++ src/main/java/com/iginicaospring/programtransito/ProgramtransitoApiApplication.java
@@ -0,0 +1,13 @@
+package com.iginicaospring.programtransito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramtransitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramtransitoApiApplication.class, args);
+	}
+
+}
