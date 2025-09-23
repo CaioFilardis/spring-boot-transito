af93c6a08257674011f32a0e328ca840b7932007
Caio Filardis <caiosantosshake@hotmail.com>
2025-09-23 10:58:44 -0300

    Aplicando o mapeamento-relacional JPA, com suas respectivas anotações que vão mapeando todo o projeto e objetos criados, assim como suas instâncias em tempo de execução.

--- src/main/java/com/iginicaospring/programtransito/domain/model/Proprietario.java
+++ src/main/java/com/iginicaospring/programtransito/domain/model/Proprietario.java
@@ -1,10 +1,27 @@
 package com.iginicaospring.programtransito.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // anotação com base no lombook instalado
+@Entity // Entity = anotação que defina a classe como entidade
+// @Table(name = "tb_proprietario") // renomear a entidade se desejável
 public class Proprietario {
 
+    @EqualsAndHashCode.Include // anotação que inclui a propriedade "Id"
+    @Id // Id = anotação que define a propriedade de identificação(primary key)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // anotação que define a estratégia nativa do banco de dados(ele mesmo faz)
     private Long id;
+
     private String nome;
     private String email;
+
+    @Column(name = "celular") // anotação que mapea exatamente a coluna deseja e renomea
     private String telefone;
 
     public Long getId() {
@@ -38,6 +55,10 @@ public class Proprietario {
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+
+
+    // Hash Code e Equals - Para comparar os objetivos
+
 
 
 }
