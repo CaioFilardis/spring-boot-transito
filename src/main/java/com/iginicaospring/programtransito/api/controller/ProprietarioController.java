8966690ce64ae942cf697a49545ba855a9a19668
Caio Filardis <caiosantosshake@hotmail.com>
2025-09-30 00:09:52 -0300

    Implementando endpoint de busca de recurso, pelo id do proprietário.

--- src/main/java/com/iginicaospring/programtransito/api/controller/ProprietarioController.java
+++ src/main/java/com/iginicaospring/programtransito/api/controller/ProprietarioController.java
@@ -7,12 +7,15 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor // criar para todas as variáveis de instância
 @RestController
@@ -24,5 +27,16 @@ public class ProprietarioController {
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
         return proprietarioRepository.findAll(); // busca todo os proprietários(em lista)
+    }
+
+    // método para realizar buscar por id
+    @GetMapping("/proprietarios/{proprietarioId}") // variável de caminho
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) { // anotação para vincular a variável de caminho
+        Optional<Proprietario> proprietario = proprietarioRepository.findById(proprietarioId);
+        if (proprietario.isPresent()) {
+            return ResponseEntity.ok(proprietario.get());
+        }
+        return ResponseEntity.notFound().build(); // retornar um erro 404
+       // return optional.orElse(null); -> obter o conteúdo dentro do Optional
     }
 }
