39a2c22fa74e4581a05f69f9e5b33f0b4a17ecff
Caio Filardis <caiosantosshake@hotmail.com>
2025-09-30 00:49:44 -0300

    Implementando endpoint de atualização de recurso, o método POST.

--- src/main/java/com/iginicaospring/programtransito/api/controller/ProprietarioController.java
+++ src/main/java/com/iginicaospring/programtransito/api/controller/ProprietarioController.java
@@ -7,11 +7,9 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Type;
 import java.util.Arrays;
@@ -35,5 +33,12 @@ public class ProprietarioController {
             return proprietarioRepository.findById(proprietarioId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    // método para adiconar novos proprietarios
+    @PostMapping // manda, adicionar novos dados
+    @ResponseStatus(HttpStatus.CREATED) // enumeração spring que retorna status de "criado"
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {// RequestBody, vincula os parâmetros no corpo da requisição
+        return proprietarioRepository.save(proprietario);
     }
 }
