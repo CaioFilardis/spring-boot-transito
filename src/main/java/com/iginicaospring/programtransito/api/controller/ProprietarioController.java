c2fb28c5d8ef6354ac2f278219dcecd1c78c1277
Caio Filardis <caiosantosshake@hotmail.com>
2025-09-30 01:06:57 -0300

    Implementando endpoint de atualização de recurso, com método PUT.

--- src/main/java/com/iginicaospring/programtransito/api/controller/ProprietarioController.java
+++ src/main/java/com/iginicaospring/programtransito/api/controller/ProprietarioController.java
@@ -41,4 +41,18 @@ public class ProprietarioController {
     public Proprietario adicionar(@RequestBody Proprietario proprietario) {// RequestBody, vincula os parâmetros no corpo da requisição
         return proprietarioRepository.save(proprietario);
     }
+
+    // método para atualizar os dados de um proprietario
+    @PutMapping("/{proprietarioId}") // variavel de caminho
+    public ResponseEntity<Proprietario> atualizar(@PathVariable Long proprietarioId,
+                                                  @RequestBody Proprietario proprietario) {
+        if (!proprietarioRepository.existsById(proprietarioId)) { // faz um select na tabela para verificar se existe
+            return ResponseEntity.notFound().build();
+        }
+
+        proprietario.setId(proprietarioId);
+        Proprietario proprietarioAtualizado = proprietarioRepository.save(proprietario);
+
+        return ResponseEntity.ok(proprietarioAtualizado);
+    }
 }
