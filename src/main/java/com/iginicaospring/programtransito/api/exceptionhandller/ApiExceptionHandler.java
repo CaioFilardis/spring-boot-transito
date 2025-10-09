582a3c638292de54de657f97f2320bc12a7aaacf
Caio Filardis <caiosantosshake@hotmail.com>
2025-10-09 00:10:14 -0300

    Customizando as informações ddo ProblemDetail de forma personalizada

--- src/main/java/com/iginicaospring/programtransito/api/exceptionhandller/ApiExceptionHandler.java
+++ src/main/java/com/iginicaospring/programtransito/api/exceptionhandller/ApiExceptionHandler.java
@@ -1,14 +1,34 @@
 package com.iginicaospring.programtransito.api.exceptionhandller;
 
 import com.iginicaospring.programtransito.domain.exception.NegocioException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-// classe que trata todas às exceções de forma global
-@RestControllerAdvice // captura exceção global
+import java.net.URI;
+
+
+@RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("http://www.programtransito.com/erros/campos-invalidos")); // url exemplo(não existe)
+
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
 
     @ExceptionHandler(NegocioException.class) // método para ser capaz de tratar exceção
     public ResponseEntity<String> capturar(NegocioException e) {
